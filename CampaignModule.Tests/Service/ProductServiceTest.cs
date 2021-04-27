92e03155af396162f9e65d79e758ad0469d0d7e1
Muhammed Gungor <cs.muhammedgungor@gmail.com>
2021-04-28 01:41:34 +0300

    xUnit project added to solution. Product services done.

---
+++ CampaignModule.Tests/Service/ProductServiceTest.cs
@@ -0,0 +1,43 @@
+﻿using CampaignModule.Service.Product;
+using CampaignModule.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CampaignModule.Tests.Service
+{
+    public class ProductServiceTest
+    {
+        private readonly IProductService _productService;
+        private readonly List<string> productCreateCommand = new List<string> { "create_product","PTest","10","100" };
+        private readonly List<string> productGetCommand = new List<string> { "get_product_info", "PTest"};
+
+        public ProductServiceTest(IProductService productService)
+        {
+            this._productService = productService;
+        }
+
+        [Fact]
+        public void Should_Insert_A_Product()
+        {
+            var response = _productService.CreateAsync(productCreateCommand);
+
+            response.Wait();
+
+            var expectedResponse = ResponseHelper.GetInstance().GetResponse(Constants.ProductConstant.CreateProductMessage, new object[] { productCreateCommand[1], productCreateCommand[2], productCreateCommand[3] });
+
+            Assert.Equal(response.Result, expectedResponse);
+        }
+
+        [Fact]
+        public void Should_Get_A_Product()
+        {
+            var response = _productService.GetAsync(productGetCommand);
+
+            response.Wait();
+
+            Assert.NotEmpty(response.Result);
+        }
+    }
+}
---
+++ CampaignModule.Tests/Startup.cs
@@ -0,0 +1,31 @@
+﻿using CampaignModule.Core.Repositories;
+using CampaignModule.Domain.Campaign;
+using CampaignModule.Domain.Order;
+using CampaignModule.Domain.Product;
+using CampaignModule.Domain.Timer;
+using CampaignModule.Service.Campaign;
+using CampaignModule.Service.Order;
+using CampaignModule.Service.Product;
+using CampaignModule.Service.Timer;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CampaignModule.Tests
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IProductService, ProductService>();
+            services.AddSingleton<IProductRepository<ProductItem>, ProductRepository>();
+            services.AddSingleton<ICampaignService, CampaignService>();
+            services.AddSingleton<ICampaignRepository<CampaignItem>, CampaignRepository>();
+            services.AddSingleton<IOrderService, OrderService>();
+            services.AddSingleton<IOrderRepository<OrderItem>, OrderRepository>();
+            services.AddSingleton<ITimerService, TimerService>();
+            services.AddSingleton<ITimerRepository<TimerItem>, TimerRepository>();
+        }
+    }
+}
